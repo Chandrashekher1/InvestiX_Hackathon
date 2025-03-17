f4172bd0222f5c36be0484b094e6c6a08cd9c64d
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2025-03-17 23:48:07 +0530

    remove chart

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -1,18 +1,9 @@
-import React from "react";
-import { LineChart, Line, XAxis, YAxis, Tooltip, CartesianGrid, ResponsiveContainer } from "recharts";
+import React from 'react'
 
-const Chart = ({ chartData }) => {
+const Chart = () => {
   return (
-    <ResponsiveContainer width="100%" height={300}>
-      <LineChart data={chartData}>
-        <XAxis dataKey="time" />
-        <YAxis />
-        <Tooltip />
-        <CartesianGrid stroke="#eee" strokeDasharray="5 5" />
-        <Line type="monotone" dataKey="price" stroke="#8884d8" />
-      </LineChart>
-    </ResponsiveContainer>
-  );
-};
+    <div>Chart</div>
+  )
+}
 
-export default Chart;
+export default Chart
