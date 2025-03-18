448f4e32b0fc8cc08a2b48a432a8f2b1f9b24491
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2025-03-18 12:39:21 +0530

    Update data

--- src/pages/RealEstate.jsx
+++ src/pages/RealEstate.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const RealEstate = () => {
   return (
     <div>
-        <h1 className='font-bold text-4xl text-center'>Future Prediction</h1>
+        <h1 className='font-bold text-4xl text-center'>Future Scope</h1>
         <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYukNB3WKs9JAar0Q--6vvD6UsY2ot5gO3FT66gC2pxdZPovdtltyr5nPrdplOjisPkvc&usqp=CAU" className='mx-[40%] my-40' alt="" />
     </div>
   )
