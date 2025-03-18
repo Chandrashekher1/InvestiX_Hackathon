985d61540bca36fbdce3ad7c99145b7804b3c317
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2025-03-18 12:42:20 +0530

    update %

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -104,7 +104,7 @@ const Home = () => {
                 <tr className="bg-gray-100">
                   <th className="p-2">Symbol</th>
                   <th className="p-2 ">Price</th>
-                  <th className="p-2 ">Change</th>
+                  <th className="p-2 ">Change%</th>
                 </tr>
               </thead>
               <tbody>
