12d02756af8529e5dfd8c219d4e1c574d0b8f2b2
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2025-03-18 15:17:16 +0530

    add real estate button

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -29,6 +29,11 @@ const Sidebar = () => {
             <img src="https://static-00.iconduck.com/assets.00/watchlist-icon-2048x1626-xp428ogc.png" alt="" className="w-6" />
             {isOpen && <Link to="/trending/" className="text-lg">Trending</Link>}
           </li>
+          <li className="flex items-center space-x-2 hover:bg-gray-700 p-2 rounded-md">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYukNB3WKs9JAar0Q--6vvD6UsY2ot5gO3FT66gC2pxdZPovdtltyr5nPrdplOjisPkvc&usqp=CAU" alt="" className="w-6" />
+            {isOpen && <Link to="/real-estate/" className="text-lg">Real Estate</Link>}
+            <img src="" alt="" />
+          </li>
         </ul>
       </nav>
     </div>
